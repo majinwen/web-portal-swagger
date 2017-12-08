b8ebdf50f8fc36aed0548cf069a96570c31ed10e
zengqingzhou <zengqingzhou@outlook.com>
2017-12-08 15:35:25 +0800

    创建index type mapping 及向es添加数据的接口

--- core-service/src/main/java/com/toutiao/web/service/repository/admin/SaveToESService.java
+++ core-service/src/main/java/com/toutiao/web/service/repository/admin/SaveToESService.java
@@ -5,5 +5,5 @@ import com.toutiao.web.dao.entity.admin.VillageEntity;
 import java.util.Map;
 
 public interface SaveToESService {
-    public Map save(String index, String type, VillageEntity village) throws Exception;
+    public void save(String index, String type, VillageEntity village) throws Exception;
 }
--- core-service/src/main/java/com/toutiao/web/service/repository/admin/impl/SaveToESServiceImpl.java
+++ core-service/src/main/java/com/toutiao/web/service/repository/admin/impl/SaveToESServiceImpl.java
@@ -19,7 +19,7 @@ import java.util.Map;
 @Service
 public class SaveToESServiceImpl implements SaveToESService {
     @Override
-    public Map save(String index, String type, VillageEntity village) throws Exception {
+    public void save(String index, String type, VillageEntity village) throws Exception {
         Settings settings = Settings.builder().put("cluster.name", "elasticsearch")
                 .build();
         Client client = new PreBuiltTransportClient(settings)
@@ -27,47 +27,10 @@ public class SaveToESServiceImpl implements SaveToESService {
         BulkRequestBuilder bulkRequest = client.prepareBulk();
         Map resultMap = new HashMap();
         JSONObject json = (JSONObject) JSONObject.toJSON(village);
-
-//        IndexRequestBuilder irb = client.prepareIndex(index,
-//                type, "1");
-//        XContentBuilder xb = XContentFactory.jsonBuilder()
-//                .startObject();
-//        xb.latlon("location", 0, 0);
-//        for (Object object : listData) {
-//            JSONObject json = JSONObject.fromObject(object);
-//            没有指定idName 那就让Elasticsearch自动生成
-        if(StringUtils.isBlank("")){
             IndexRequestBuilder lrb = client
                     .prepareIndex(index, type)
                     .setSource(json);
             bulkRequest.add(lrb);
-        }
-        else{
-//                String idValue = json.optString(idName);
-            IndexRequestBuilder lrb = client
-                    .prepareIndex(index, type,"003")
-                    .setSource(json);
-            bulkRequest.add(lrb);
-        }
-//        IndexRequestBuilder lrb = client
-//                        .prepareIndex(index, type)
-//                        .setSource(json);
-//        bulkRequest.add(lrb);
-////
-//        }
-//        BulkResponse bulkResponse = bulkRequest.execute().actionGet();
-//        if (bulkResponse.hasFailures()) {
-//            // process failures by iterating through each bulk response item
-//            System.out.println(bulkResponse.getItems().toString());
-//            resultMap.put("500", "保存ES失败!");
-//            return resultMap;
-//        }
-//        bulkRequest = ESTools.client.prepareBulk();
-//        resultMap.put("200", "保存ES成功!");
-//        xb.endObject();
-//        irb.setSource(xb);
-//        BulkResponse bulkResponse = brb.execute().actionGet();
         BulkResponse bulkResponse = bulkRequest.execute().actionGet();
-        return null;
     }
 }
