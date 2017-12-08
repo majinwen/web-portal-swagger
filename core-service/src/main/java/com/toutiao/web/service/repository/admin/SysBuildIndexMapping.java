7d185f04d574ef20af0e4a2cf98dd8d5a7d3cbc5
zengqingzhou <zengqingzhou@outlook.com>
2017-12-08 12:44:31 +0800

    创建index type mapping 及向es添加数据

---
+++ core-service/src/main/java/com/toutiao/web/service/repository/admin/SysBuildIndexMapping.java
@@ -0,0 +1,10 @@
+package com.toutiao.web.service.repository.admin;
+
+import com.toutiao.web.dao.entity.admin.village;
+
+import java.util.List;
+import java.util.Map;
+
+public interface SysBuildIndexMapping {
+    public void buildIndexMapping() throws Exception;
+}
