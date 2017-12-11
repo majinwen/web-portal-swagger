cc8f3bc976de0b3205b1c924357118769ae28dfe
zengqingzhou <zengqingzhou@outlook.com>
2017-12-11 14:45:28 +0800

    wwww

--- core-service/src/main/java/com/toutiao/web/service/repository/admin/SaveToESService.java
+++ core-service/src/main/java/com/toutiao/web/service/repository/admin/SaveToESService.java
@@ -2,8 +2,7 @@ package com.toutiao.web.service.repository.admin;
 
 import com.toutiao.web.dao.entity.admin.VillageEntity;
 
-import java.util.Map;
 
 public interface SaveToESService {
-    public void save(String index, String type, VillageEntity village) throws Exception;
+     Boolean save(String index, String type, VillageEntity village) throws Exception;
 }
