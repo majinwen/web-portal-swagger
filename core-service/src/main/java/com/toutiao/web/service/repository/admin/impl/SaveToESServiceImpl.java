5fde3e65e25243a64a7b74fe81ec3029ace6846a
zengqingzhou <zengqingzhou@outlook.com>
2017-12-11 15:55:30 +0800

    保存数据到es

--- core-service/src/main/java/com/toutiao/web/service/repository/admin/impl/SaveToESServiceImpl.java
+++ core-service/src/main/java/com/toutiao/web/service/repository/admin/impl/SaveToESServiceImpl.java
@@ -3,7 +3,6 @@ package com.toutiao.web.service.repository.admin.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.toutiao.web.dao.entity.admin.VillageEntity;
 import com.toutiao.web.service.repository.admin.SaveToESService;
-import org.apache.commons.lang.StringUtils;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequestBuilder;
@@ -14,16 +13,16 @@ import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.stereotype.Service;
 
 import java.net.InetAddress;
-import java.util.HashMap;
-import java.util.Map;
+
 @Service
 public class SaveToESServiceImpl implements SaveToESService {
     @Override
-    public void save(String index, String type, VillageEntity village) throws Exception {
+    public Boolean save(String index, String type, VillageEntity village) throws Exception {
         Settings settings = Settings.builder().put("cluster.name", "elasticsearch")
                 .build();
         Client client = new PreBuiltTransportClient(settings)
                 .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("47.104.96.88"), 9300));
+        Boolean flag = true;
         BulkRequestBuilder bulkRequest = client.prepareBulk();
         JSONObject json = (JSONObject) JSONObject.toJSON(village);
             IndexRequestBuilder lrb = client
@@ -31,5 +30,10 @@ public class SaveToESServiceImpl implements SaveToESService {
                     .setSource(json);
             bulkRequest.add(lrb);
         BulkResponse bulkResponse = bulkRequest.execute().actionGet();
+        // 如果失败
+        if(bulkResponse.hasFailures()){
+            flag = false;
+        }
+        return flag;
     }
 }
