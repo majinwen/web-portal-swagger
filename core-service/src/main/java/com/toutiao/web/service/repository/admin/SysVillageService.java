01a58a507805b2fb101ef7ccb09394f6db583eef
zengqingzhou <zengqingzhou@outlook.com>
2017-12-11 15:59:07 +0800

    创建mapping

--- core-service/src/main/java/com/toutiao/web/service/repository/admin/SysVillageService.java
+++ core-service/src/main/java/com/toutiao/web/service/repository/admin/SysVillageService.java
@@ -1,12 +1,13 @@
 package com.toutiao.web.service.repository.admin;
 
 import com.toutiao.web.dao.entity.admin.VillageEntity;
+import com.toutiao.web.domain.app.VillageRequest;
 
 import java.net.UnknownHostException;
 import java.util.List;
 
 public interface SysVillageService {
     List GetNearByhHouseAndDistance(String index, String type, double lat, double lon, String distance) throws Exception;
-    VillageEntity findVillageById(String index, String type,Integer id) throws UnknownHostException;
 
+    List findVillageByConditions(String index, String type, VillageRequest villageRequest);
 }
