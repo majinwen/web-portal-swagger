00a9c590d65eb7190a64d0e4f0cf0cd7e6c42c95
zengqingzhou <zengqingzhou@outlook.com>
2017-12-15 09:55:57 +0800

    创建mapping

--- api-impl/src/main/java/com/toutiao/web/apiimpl/controller/sys/SaveToESController.java
+++ api-impl/src/main/java/com/toutiao/web/apiimpl/controller/sys/SaveToESController.java
@@ -8,8 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class SaveToESController {
