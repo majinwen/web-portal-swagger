4195583c2be1f73211d4be6243255637c5f56c9a
jiangweilong <jiangweilongowen@163.com>
2017-12-18 14:13:42 +0800

    新增智能找房基础方法

--- api-impl/src/main/java/com/toutiao/web/apiimpl/impl/Intelligence/IntelligenceFindHouseController.java
+++ api-impl/src/main/java/com/toutiao/web/apiimpl/impl/Intelligence/IntelligenceFindHouseController.java
@@ -1,5 +1,11 @@
 package com.toutiao.web.apiimpl.impl.Intelligence;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ifh")
 public class IntelligenceFindHouseController {
 
 
