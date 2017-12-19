9c4a92d14f22424d68d05d42b975574bda794db5
jiangweilong <jiangweilongowen@163.com>
2017-12-19 11:07:39 +0800

    构建es连接

--- common/src/main/java/com/toutiao/web/common/util/ESClientTools.java
+++ common/src/main/java/com/toutiao/web/common/util/ESClientTools.java
@@ -1,17 +1,16 @@
 package com.toutiao.web.common.util;
 
 import lombok.Data;
-import org.apache.http.HttpHost;
-import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Component
 @Data
@@ -22,13 +21,15 @@ public class ESClientTools {
     @Value("${es.cluster.name}")
     private String esClusterName = "elasticsearch";//集群名
     @Value("${es.server.ips}")
-    private String esServerIps = "47.104.96.88:9300:http";//集群服务IP集合
+    private String esServerIps = "47.104.96.88:9300";//集群服务IP集合
 
-    private volatile RestHighLevelClient client;
+    private volatile TransportClient client;
+
 
     Settings settings = Settings.builder().put("cluster.name", esClusterName).build();
 
-    public RestHighLevelClient init(){
+    public TransportClient init(){
+
 
         if(client == null){
             synchronized (TransportClient.class){}
@@ -37,30 +38,16 @@ public class ESClientTools {
                     String[] addresses = esServerIps.split(",");
                     for (String address : addresses) {
                         String[] hostAndPort = address.split(":");
-                        client = new RestHighLevelClient(
-                                RestClient.builder(
-                                        new HttpHost(hostAndPort[0], Integer.valueOf(hostAndPort[1]), hostAndPort[2])));
+                        client = new PreBuiltTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(hostAndPort[0]), Integer.valueOf(hostAndPort[1])));
                     }
-                } catch (Exception e) {
+                } catch (UnknownHostException e) {
                     if (client != null) {
-                        try {
-                            client.close();
-                        } catch (IOException e1) {
-                            e1.printStackTrace();
-                        }
+                        client.close();
                     }
                 }
             }
         }
         return client;
     }
-
-    /**
-     * 创建索引
-     * @param indexName
-     * @param shards
-     * @param replicas
-     * @return
-     */
 
 }
