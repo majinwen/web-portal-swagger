d0f1848dc5803deeb7599a639338743c80c83455
jiangweilong <jiangweilongowen@163.com>
2017-12-27 12:49:03 +0800

    小区地铁api

--- api-impl/src/main/resources/static/js/plot-detail-map-message.js
+++ api-impl/src/main/resources/static/js/plot-detail-map-message.js
@@ -93,7 +93,7 @@ $('.map-message-btn').on('click', 'li', function () {
         $(this).prevAll().addClass('choose');
         $(this).nextAll().removeClass('choose');
     }
-    var url = 'http://api.map.baidu.com/place/v2/search?query=' + text + '&location=39.915,116.404&radius=3000&scope=2&page_size=5&distance&output=json&ak=UrflQIXBCuEZUVkwxgC3xE5y8rRPpjpS';
+    var url = 'http://api.map.baidu.com/place/v2/search?query=' + text + '&tag='+ text +'&location=39.915,116.404&radius=3000&scope=2&page_size=5&distance&output=json&ak=UrflQIXBCuEZUVkwxgC3xE5y8rRPpjpS';
     $.ajax({
         type: 'GET',
         url: url,
