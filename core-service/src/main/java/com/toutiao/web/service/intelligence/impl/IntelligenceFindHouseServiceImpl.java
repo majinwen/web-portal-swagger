116439a404c6a0bcb28898f7b86174fd984cd64e
zengqingzhou <zengqingzhou@outlook.com>
2017-12-27 15:02:15 +0800

    展示小区详情和小区列表12

--- core-service/src/main/java/com/toutiao/web/service/intelligence/impl/IntelligenceFindHouseServiceImpl.java
+++ core-service/src/main/java/com/toutiao/web/service/intelligence/impl/IntelligenceFindHouseServiceImpl.java
@@ -34,6 +34,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 
 import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
@@ -105,26 +106,124 @@ public class IntelligenceFindHouseServiceImpl implements IntelligenceFindHouseSe
         //判断类型
         //类型2A
         if ("4".equals(intelligenceQuery.getUserType())){
-            List finalList = new ArrayList();
             List<IntelligenceFindhouse> list = intelligenceFindhouseMapper.selectByTypeTwoA(intelligenceQuery);
-            for (int i = 0;i<list.size();i++){
-                for (int j = 0; j <list.size();j++){
-
-                }
-            }
-            return null;
+            List<IntelligenceFindhouse> finalList = recommendHouse(list);
+            return finalList;
         }
         //类型2B
         if ("5".equals(intelligenceQuery.getUserType())){
             List<IntelligenceFindhouse> list = intelligenceFindhouseMapper.selectByTypeTwoB(intelligenceQuery);
-            return null;
+            List<IntelligenceFindhouse> finalList = recommendHouse(list);
+            return finalList;
         }
         //类型2C
         if ("6".equals(intelligenceQuery.getUserType())){
             List<IntelligenceFindhouse> list = intelligenceFindhouseMapper.selectByTypeTwoC(intelligenceQuery);
-            return null;
+            List<IntelligenceFindhouse> finalList = recommendHouse(list);
+            return finalList;
         }
 
         return null;
     }
+
+    /**
+     *
+     * 功能描述：筛选方法
+     * @author zengqingzhou
+     * @date 2017/12/27 14:58
+     * @param [list]
+     * @return java.util.List<com.toutiao.web.dao.entity.officeweb.IntelligenceFindhouse>
+     */
+    public List<IntelligenceFindhouse> recommendHouse(List<IntelligenceFindhouse> list){
+        List finalList = new ArrayList();
+        BigDecimal tempPrice = null;
+        Short tempRingRoad = null;
+        Integer tempStarProperty = null;
+        //均价最低
+        if (list.size() > 1){
+            for (int i = 0;i<list.size()-1;i++){
+                for (int j = 0; j <list.size()-1-i;j++){
+
+                    double esfPriceJ= Double.parseDouble(null);
+                    double esfPriceJ_1= Double.parseDouble(null);
+                    if (null!=list.get(j).getEsfPrice()&&list.get(j).getEsfPrice().doubleValue()>0){
+                        esfPriceJ= list.get(j).getEsfPrice().doubleValue();
+                    }
+                    if (null!=list.get(j).getPrice()&&list.get(j).getPrice().doubleValue()>0){
+                        esfPriceJ= list.get(j).getPrice().doubleValue();
+                    }
+                    if (null!=list.get(j+1).getEsfPrice()&&list.get(j+1).getEsfPrice().doubleValue()>0){
+                        esfPriceJ_1= list.get(j+1).getEsfPrice().doubleValue();
+                    }
+                    if (null!=list.get(j+1).getPrice()&&list.get(j+1).getPrice().doubleValue()>0){
+                        esfPriceJ_1= list.get(j+1).getPrice().doubleValue();
+                    }
+
+                    if (esfPriceJ>esfPriceJ_1){
+                        tempPrice = list.get(j).getEsfPrice();
+                        list.get(j).setEsfPrice(list.get(j+1).getEsfPrice());
+                        list.get(j+1).setEsfPrice(tempPrice);
+                    }
+                }
+            }
+
+            finalList.add(list.get(1));
+            finalList.add(list.get(2));
+            list.remove(0);
+            list.remove(0);
+        }
+
+        //环线最小
+        if (list.size()>0){
+            for (int i = 0;i<list.size()-1;i++){
+                for (int j = 0; j <list.size()-1-i;j++){
+
+                    short ringRoad = Short.parseShort(null);
+                    short ringRoad_1 = Short.parseShort(null);
+                    if (null!=list.get(j).getRingRoad()&&list.get(j).getRingRoad()>0){
+                        ringRoad= list.get(j).getRingRoad();
+                    }
+
+                    if (null!=list.get(j+1).getRingRoad()&&list.get(j+1).getRingRoad()>0){
+                        ringRoad_1= list.get(j+1).getRingRoad();
+                    }
+
+                    if (ringRoad>ringRoad_1){
+                        tempRingRoad = list.get(j).getRingRoad();
+                        list.get(j).setRingRoad(list.get(j+1).getRingRoad());
+                        list.get(j+1).setRingRoad(tempRingRoad);
+                    }
+                }
+            }
+            finalList.add(list.get(1));
+            list.remove(0);
+        }
+
+        //明星楼盘
+        if (list.size()>1){
+            for (int i = 0;i<list.size()-1;i++){
+                for (int j = 0; j <list.size()-1-i;j++){
+
+                    Integer starProperty = null;
+                    Integer starProperty_1 = null;
+                    if (null!=list.get(j).getStarProperty()&&list.get(j).getStarProperty()>0){
+                        starProperty= list.get(j).getStarProperty();
+                    }
+
+                    if (null!=list.get(j+1).getStarProperty()&&list.get(j+1).getStarProperty()>0){
+                        starProperty_1= list.get(j+1).getStarProperty();
+                    }
+
+                    if (starProperty>starProperty_1){
+                        tempStarProperty = list.get(j).getStarProperty();
+                        list.get(j).setStarProperty(list.get(j+1).getStarProperty());
+                        list.get(j+1).setStarProperty(tempStarProperty);
+                    }
+                }
+            }
+            finalList.add(list.get(1));
+        }
+    return finalList;
+    }
+
 }
