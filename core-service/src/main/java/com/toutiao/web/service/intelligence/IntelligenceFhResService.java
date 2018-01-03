ebbf98712b5b36e040b8ba1b8b22a0f964ea1986
zengqingzhou <zengqingzhou@outlook.com>
2018-01-03 18:06:03 +0800

    保存数据报告页数据

--- core-service/src/main/java/com/toutiao/web/service/intelligence/IntelligenceFhResService.java
+++ core-service/src/main/java/com/toutiao/web/service/intelligence/IntelligenceFhResService.java
@@ -2,8 +2,12 @@ package com.toutiao.web.service.intelligence;
 
 import com.toutiao.web.dao.entity.officeweb.IntelligenceFhRes;
 
+import java.util.List;
+
 public interface IntelligenceFhResService {
 
     IntelligenceFhRes queryUserReport(String usePhone);
+
+    List<IntelligenceFhRes> queryResById(Integer id);
 
 }
