f57fd3f1f19318c3cb76024017614a07386db93b
wushoulei <wushoulei@uban.com>
2018-01-04 21:15:06 +0800

    报告页价格走势，供需情况

--- api-impl/src/main/resources/static/js/intelligent-choose.js
+++ api-impl/src/main/resources/static/js/intelligent-choose.js
@@ -1,15 +1,18 @@
 $(function () {
     $('#superContainer').fullpage({
         fitToSection: true,
+        resize: false,
         onLeave: function (index, nextIndex, direction) {
             if (nextIndex == 4 && direction == 'down') {
                 console.log(joinParams(options));
                 $.ajax({
                     type: "GET",
+                    async: true,
                     url: router_city('/findhouse/showUserPortrayal'),
                     data: options,
                     success: function (data) {
                         // console.log(data);
+                        // $("#button_report").attr("href", router_city('/findhouse/showUserPortrayal') + joinParams(options));
                     }
                 })
             }
@@ -24,6 +27,8 @@ $(function () {
     chooseUserType();   // 用户选择类型
 
     chooseUserFinds();  // 用户开启智能找房之旅
+
+    // reportEchartAssemble(); // 报考页图表集合
 });
 
 /**
@@ -168,7 +173,6 @@ function chooseUserFinds() {
                     //将第七种画像附给当前用户userPortrayalType
                     if (options['userType'] == 3) {
                         options['userPortrayalType'] = 7;
-                        $("#button_report").attr("href", router_city('/findhouse/showUserPortrayal') + joinParams(options));
                     }
                 }
             });
@@ -194,7 +198,6 @@ function chooseUserFinds() {
                     //将第七种画像附给当前用户userPortrayalType
                     if (options['userType'] == 3) {
                         options['userPortrayalType'] = 7;
-                        $("#button_report").attr("href", router_city('/findhouse/showUserPortrayal') + joinParams(options));
                     }
                 }
             });
@@ -286,7 +289,6 @@ function chooseUserFinds() {
                     if (options['layOut'] == 1) {
                         options['schoolFlag'] = 0;
                         options['hospitalFlag'] = 0;
-                        $("#button_report").attr("href", router_city('/findhouse/showUserPortrayal') + joinParams(options));
                     }
                 }
             })
@@ -337,7 +339,6 @@ function chooseUserFinds() {
         var familyHtml = '<p><span>孩子：<em>'+ $('#hasChild').find('li.current').find('span').text() +'</em></span>' +
                          '<span>老人：<em>' + $('#oldMan').find('li.current').find('span').text() +'</em></span></p>';
         $('.list-item').find('li').eq(3).find('.result-animate').html(familyHtml);
-        $("#button_report").attr("href", router_city('/findhouse/showUserPortrayal') + joinParams(options));
     });
 }
 
