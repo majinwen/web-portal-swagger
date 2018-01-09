c2b2af4b512ccffe1fa25759548eb2197785cec1
zhw <zhanghongwei@toutiaofangchan.com>
2018-01-09 19:16:25 +0800

    zhw

--- api-impl/src/main/java/com/toutiao/web/apiimpl/impl/Intelligence/IntelligenceFindHouseController.java
+++ api-impl/src/main/java/com/toutiao/web/apiimpl/impl/Intelligence/IntelligenceFindHouseController.java
@@ -209,24 +209,6 @@ public class IntelligenceFindHouseController {
         return NashResult.build(userType);
     }
 
-    /**
-     * 功能描述：异步根据价钱获取小区数量与相应的比率
-     *
-     * @param intelligenceQuery
-     * @return com.toutiao.web.common.restmodel.NashResult
-     * @author zhw
-     * @date 2017/12/18 21:05
-     */
-    @RequestMapping("/goCheckPrice")
-    @ResponseBody
-    public NashResult plotCountByTotalPrice(IntelligenceQuery intelligenceQuery) {
-        IntelligenceFh intelligenceFh = intelligenceFindHouseService.queryUserCheckPrice(intelligenceQuery);
-        if (intelligenceFh.getPlotCount() - 5 < 5) {
-            intelligenceFh.setPlotCount(0);
-        }
-        //获取根据用户条件筛选的小区数量和相应比率
-        return NashResult.build(intelligenceFh);
-    }
 //    @RequestMapping("/goCheckPrice")
 //    @ResponseBody
 //    public NashResult plotCountByTotalPrice(IntelligenceQuery intelligenceQuery) {
