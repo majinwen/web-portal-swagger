1c52c775ef651180a15299a378759f34e3f43e0f
jiangweilong <jiangweilongowen@163.com>
2018-03-20 17:36:04 +0800

    修改同步bug

--- core-service/src/main/java/com/toutiao/web/service/rent/impl/RentHouseServiceImpl.java
+++ core-service/src/main/java/com/toutiao/web/service/rent/impl/RentHouseServiceImpl.java
@@ -184,12 +184,13 @@ public class RentHouseServiceImpl implements RentHouseService{
             if (rentHouseQuery.getRentSign()==0){
                 boolQueryBuilder.must(QueryBuilders.termQuery("rent_sign",RENT));
             }
-            if (rentHouseQuery.getRentSign()==1){
-                boolQueryBuilder.must(QueryBuilders.termQuery("rent_sign",DISPERSED_APARTMENTS));
+            if (rentHouseQuery.getRentSign()==1 || rentHouseQuery.getRentSign()==2){
+                BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+                boolQueryBuilder.must(queryBuilder.should(QueryBuilders.termQuery("rent_sign",1)).should(QueryBuilders.termQuery("rent_sign",2)));
             }
-            if (rentHouseQuery.getRentSign()==2){
-                boolQueryBuilder.must(QueryBuilders.termQuery("rent_sign",FOCUS_APARTMENT));
-            }
+//            if (rentHouseQuery.getRentSign()==2){
+//                boolQueryBuilder.must(QueryBuilders.termQuery("rent_sign",FOCUS_APARTMENT));
+//            }
             srb.setSize(4);
             SearchResponse searchResponse = srb.setQuery(boolQueryBuilder).execute().actionGet();
             SearchHit[] hits = searchResponse.getHits().getHits();
@@ -232,7 +233,7 @@ public class RentHouseServiceImpl implements RentHouseService{
             SearchHit[] hits = searchResponse.getHits().getHits();
             if (hits.length>0){
                 long time = new Date().getTime();
-                long index = (time / 600000) % hits.length;
+                long index = (time / 60000) % hits.length;
                 Map result = hits[(int) index].getSource();
                 return result;
             }
