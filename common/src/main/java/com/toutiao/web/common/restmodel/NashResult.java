c4263e2feb36e512330610bea674ec8d9d346b78
张晶磊 <zjl_wzw@163.com>
2018-03-21 17:05:14 +0800

    restful api

--- common/src/main/java/com/toutiao/web/common/restmodel/NashResult.java
+++ common/src/main/java/com/toutiao/web/common/restmodel/NashResult.java
@@ -65,23 +65,6 @@ public class NashResult<T> implements Serializable {
     private String msg="";
     /**数据*/
     private Object data;
-    public String getCode() {
-        return code;
-    }
-    public void setCode(String status) {
-        this.code = status;
-    }
-    public String getMsg() {
-        return msg;
-    }
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-    public Object getData() {
-        return data;
-    }
-    public void setData(Object data) {
-        this.data = data;
-    }
+
 
 }
