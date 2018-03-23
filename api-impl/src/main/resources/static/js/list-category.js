7e6bb220098305733f154628b9ce7df1a1cdf024
jiangweilong <jiangweilongowen@163.com>
2018-03-23 11:55:52 +0800

    小区地铁

--- api-impl/src/main/resources/static/js/list-category.js
+++ api-impl/src/main/resources/static/js/list-category.js
@@ -1435,21 +1435,24 @@ function pullUpAction() {
                                     var _subwayObj = dataCon[i]['metroWithPlotsDistance'];
                                     var _key = dataCon[i]['key'];
                                     if (_subwayObj && _key) {
-                                        var _subwayArray = _subwayObj[_key].split('$');
-                                        if (_subwayArray.length > 2) {
-                                            var _subwayDesc;
+                                        if( _subwayObj[_key]!=undefined){
+                                            var _subwayArray = _subwayObj[_key].split('$');
+                                            if (_subwayArray.length > 2) {
+                                                var _subwayDesc;
 
-                                            var _distance = parseInt(_subwayArray[2]);
-                                            if (_distance > 1000) {
-                                                var _tempDistance = parseFloat(_distance / 1000).toFixed(1);
-                                                _subwayDesc = "距离" + _subwayArray[1] + "[" + _subwayArray[0] + "] "
-                                                    + parseFloat(_tempDistance) + "km";
-                                            } else {
-                                                _subwayDesc = "距离" + _subwayArray[1] + "[" + _subwayArray[0] + "] "
-                                                    + _distance + "m";
+                                                var _distance = parseInt(_subwayArray[2]);
+                                                if (_distance > 1000) {
+                                                    var _tempDistance = parseFloat(_distance / 1000).toFixed(1);
+                                                    _subwayDesc = "距离" + _subwayArray[1] + "[" + _subwayArray[0] + "] "
+                                                        + parseFloat(_tempDistance) + "km";
+                                                } else {
+                                                    _subwayDesc = "距离" + _subwayArray[1] + "[" + _subwayArray[0] + "] "
+                                                        + _distance + "m";
+                                                }
+                                                dataCon[i]['subwayDesc'] = _subwayDesc;
                                             }
-                                            dataCon[i]['subwayDesc'] = _subwayDesc;
                                         }
+
                                     }
                                 }
 
@@ -1638,21 +1641,24 @@ function pullDownAction() {
                                         var _subwayObj = dataCon[i]['metroWithPlotsDistance'];
                                         var _key = dataCon[i]['key'];
                                         if (_subwayObj && _key) {
-                                            var _subwayArray = _subwayObj[_key].split('$');
-                                            if (_subwayArray.length > 2) {
-                                                var _subwayDesc;
+                                            if( _subwayObj[_key]!=undefined){
+                                                var _subwayArray = _subwayObj[_key].split('$');
+                                                if (_subwayArray.length > 2) {
+                                                    var _subwayDesc;
 
-                                                var _distance = parseInt(_subwayArray[2]);
-                                                if (_distance > 1000) {
-                                                    var _tempDistance = parseFloat(_distance / 1000).toFixed(1);
-                                                    _subwayDesc = "距离" + _subwayArray[1] + "[" + _subwayArray[0] + "] "
-                                                        + parseFloat(_tempDistance) + "km";
-                                                } else {
-                                                    _subwayDesc = "距离" + _subwayArray[1] + "[" + _subwayArray[0] + "] "
-                                                        + _distance + "m";
+                                                    var _distance = parseInt(_subwayArray[2]);
+                                                    if (_distance > 1000) {
+                                                        var _tempDistance = parseFloat(_distance / 1000).toFixed(1);
+                                                        _subwayDesc = "距离" + _subwayArray[1] + "[" + _subwayArray[0] + "] "
+                                                            + parseFloat(_tempDistance) + "km";
+                                                    } else {
+                                                        _subwayDesc = "距离" + _subwayArray[1] + "[" + _subwayArray[0] + "] "
+                                                            + _distance + "m";
+                                                    }
+                                                    dataCon[i]['subwayDesc'] = _subwayDesc;
                                                 }
-                                                dataCon[i]['subwayDesc'] = _subwayDesc;
                                             }
+
                                         }
                                     }
 
