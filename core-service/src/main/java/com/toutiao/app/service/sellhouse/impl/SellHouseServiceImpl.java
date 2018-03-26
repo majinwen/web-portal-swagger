7a120f70219ce272334b2273dddc637ed1b9682d
jiangweilong <jiangweilongowen@163.com>
2018-03-26 13:01:09 +0800

    抖房app工程架构

--- core-service/src/main/java/com/toutiao/app/service/sellhouse/impl/SellHouseServiceImpl.java
+++ core-service/src/main/java/com/toutiao/app/service/sellhouse/impl/SellHouseServiceImpl.java
@@ -4,10 +4,8 @@ package com.toutiao.app.service.sellhouse.impl;
 import com.toutiao.app.dao.sellhouse.SellHouseEsDao;
 import com.toutiao.app.service.sellhouse.SellHouseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
-@Configuration
 @Service
 public class SellHouseServiceImpl implements SellHouseService{
 
--- dao/src/main/java/com/toutiao/app/dao/sellhouse/impl/SellHouseEsDaoImpl.java
+++ dao/src/main/java/com/toutiao/app/dao/sellhouse/impl/SellHouseEsDaoImpl.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
-@Configuration
 @Service
 public class SellHouseEsDaoImpl implements SellHouseEsDao{
 
