b95116c9e20b0b3db8cf3e33c1b2d3edc50d2b89
李乐 <lile@toutiaofangchan.com>
2018-04-24 19:16:59 +0800

    修改bug

--- core-service/src/main/java/com/toutiao/app/service/favorite/impl/FavoriteRestServiceImpl.java
+++ core-service/src/main/java/com/toutiao/app/service/favorite/impl/FavoriteRestServiceImpl.java
@@ -1,12 +1,18 @@
 package com.toutiao.app.service.favorite.impl;
 
 import com.toutiao.app.service.favorite.FavoriteRestService;
+import com.toutiao.app.service.newhouse.impl.NewHouseRestServiceImpl;
 import com.toutiao.web.dao.mapper.officeweb.favorite.UserFavoriteNewHouseMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FavoriteRestServiceImpl implements FavoriteRestService {
+
+
+    private Logger logger = LoggerFactory.getLogger(FavoriteRestServiceImpl.class);
     @Autowired
     private UserFavoriteNewHouseMapper userFavoriteNewHouseMapper;
 
@@ -20,7 +26,7 @@ public class FavoriteRestServiceImpl implements FavoriteRestService {
             return favoriteCount;
         }catch (Exception e)
         {
-
+            logger.error("获取新房收藏异常"+newCode.toString()+"={}",e.getStackTrace());
         }
        return  favoriteCount;
     }
