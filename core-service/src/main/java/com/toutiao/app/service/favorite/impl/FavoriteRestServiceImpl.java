4367dffa4914cbb9958647518ba8faf3af4a82bd
李乐 <lile@toutiaofangchan.com>
2018-04-24 19:12:17 +0800

    修改bug

--- core-service/src/main/java/com/toutiao/app/service/favorite/impl/FavoriteRestServiceImpl.java
+++ core-service/src/main/java/com/toutiao/app/service/favorite/impl/FavoriteRestServiceImpl.java
@@ -1,16 +1,27 @@
 package com.toutiao.app.service.favorite.impl;
 
 import com.toutiao.app.service.favorite.FavoriteRestService;
+import com.toutiao.web.dao.mapper.officeweb.favorite.UserFavoriteNewHouseMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FavoriteRestServiceImpl implements FavoriteRestService {
     @Autowired
-    private
+    private UserFavoriteNewHouseMapper userFavoriteNewHouseMapper;
+
 
     @Override
-    public Integer newHouseFavoriteByNewCode(Integer newCode) {
+    public Integer newHouseFavoriteByNewCode(Integer newCode)
+    {
+        int favoriteCount=0;
+        try {
+           favoriteCount= userFavoriteNewHouseMapper.newHouseFavoriteByNewCode(newCode);
+            return favoriteCount;
+        }catch (Exception e)
+        {
 
+        }
+       return  favoriteCount;
     }
 }
