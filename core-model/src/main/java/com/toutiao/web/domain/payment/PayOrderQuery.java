70e51c8248329a61f204cd48ec435a7287618872
lile <lile@toutiaofangchan.com>
2018-05-31 18:50:08 +0800

    修改bug

---
+++ core-model/src/main/java/com/toutiao/web/domain/payment/PayOrderQuery.java
@@ -0,0 +1,12 @@
+package com.toutiao.web.domain.payment;
+
+import lombok.Data;
+
+@Data
+public class PayOrderQuery {
+    private  Integer pageNum=1;
+
+    private  Integer pageSize=5;
+
+    private  Integer type;
+}
