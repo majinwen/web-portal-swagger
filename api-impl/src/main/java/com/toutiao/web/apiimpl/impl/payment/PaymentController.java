f539442cbe74055853644653e6d9467b243d29e5
jiangweilong <jiangweilongowen@163.com>
2018-06-04 20:54:24 +0800

    maven

--- api-impl/src/main/java/com/toutiao/web/apiimpl/impl/payment/PaymentController.java
+++ api-impl/src/main/java/com/toutiao/web/apiimpl/impl/payment/PaymentController.java
@@ -12,12 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 
 @Controller
