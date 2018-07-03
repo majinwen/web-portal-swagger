b59d43670e535634855a46f3f1c963abc1ac3e9d
zhaiyanming <zhaiyanming1992@163.com>
2018-07-03 11:35:54 +0800

    修改字段名称

--- core-service/src/main/java/com/toutiao/app/service/homepage/impl/HomePageServiceImpl.java
+++ core-service/src/main/java/com/toutiao/app/service/homepage/impl/HomePageServiceImpl.java
@@ -158,7 +158,7 @@ public class HomePageServiceImpl implements HomePageRestService {
         //构建筛选器
         BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
         //筛选低价房 isCutPrice=1
-        boolQueryBuilder.must(QueryBuilders.termQuery("isCutPrice", 1));
+		boolQueryBuilder.must(QueryBuilders.termQuery("price_increase_decline", 2));
         SearchResponse cutPriceHouses = homePageEsDao.getHomePageCutPrice(boolQueryBuilder);
         SearchHit[] hits = cutPriceHouses.getHits().getHits();
         List<HomePageCutPriceDo> homePageCutPriceDos = new ArrayList<>();
