88c3788216a8b862fb3379547c4ffe7cdb778cfb
admin <873188106@qq.com>
2018-07-04 15:33:18 +0800

    11

--- core-service/src/main/java/com/toutiao/app/service/subscribe/impl/SubscribeServiceImpl.java
+++ core-service/src/main/java/com/toutiao/app/service/subscribe/impl/SubscribeServiceImpl.java
@@ -1,10 +1,14 @@
 package com.toutiao.app.service.subscribe.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.toutiao.app.domain.sellhouse.CutPriceShellHouseDoQuery;
+import com.toutiao.app.domain.sellhouse.LowerPriceShellHouseDoQuery;
 import com.toutiao.app.domain.sellhouse.SellHouseBeSureToSnatchDoQuery;
 import com.toutiao.app.domain.sellhouse.SellHouseBeSureToSnatchDomain;
 import com.toutiao.app.domain.subscribe.UserSubscribeDetailDo;
 import com.toutiao.app.domain.subscribe.UserSubscribeListDo;
+import com.toutiao.app.service.sellhouse.CutPriceSellHouseRestService;
+import com.toutiao.app.service.sellhouse.LowerPriceSellHouseRestService;
 import com.toutiao.app.service.sellhouse.SellHouseService;
 import com.toutiao.app.service.subscribe.SubscribeService;
 import com.toutiao.web.dao.entity.subscribe.UserSubscribe;
@@ -22,6 +26,10 @@ public class SubscribeServiceImpl implements SubscribeService {
     UserSubscribeMapper userSubscribeMapper;
     @Autowired
     private SellHouseService sellHouseService;
+    @Autowired
+    private CutPriceSellHouseRestService cutPriceSellHouseRestService;
+    @Autowired
+    private LowerPriceSellHouseRestService lowerPriceSellHouseRestService;
 
     @Override
     public int deleteByPrimaryKey(Integer id) {
@@ -59,8 +67,7 @@ public class SubscribeServiceImpl implements SubscribeService {
     }
 
     /**
-     *
-     * @param userId 用户ID
+     * @param userId           用户ID
      * @param isGetHouseDetail 是否获取符合订阅条件的房源详情
      * @return
      */
@@ -74,7 +81,7 @@ public class SubscribeServiceImpl implements SubscribeService {
             userSubscribeListDo.setUserSubscribeDetail(userSubscribeDetailDo);
             //填充新增数量
             userSubscribeListDo.setNewCount(getNewCountBySubscribe(userSubscribeDetailDo));
-            if(isGetHouseDetail) {
+            if (isGetHouseDetail) {
                 //填充房源列表数据
                 userSubscribeListDo.setHouseList(getHouseListBySubscribe(userSubscribeDetailDo));
             }
@@ -83,23 +90,24 @@ public class SubscribeServiceImpl implements SubscribeService {
         return userSubscribeListDoList;
     }
 
-    private Long getNewCountBySubscribe(UserSubscribeDetailDo userSubscribeDetailDo)
-    {
-        if(userSubscribeDetailDo.getTopicType() == 3) {
+    private Long getNewCountBySubscribe(UserSubscribeDetailDo userSubscribeDetailDo) {
+        Integer pageIndex = 1;
+        Integer pageSize = 1;
+        if (userSubscribeDetailDo.getTopicType() == 3) {
             SellHouseBeSureToSnatchDoQuery sellHouseBeSureToSnatchDoQuery = new SellHouseBeSureToSnatchDoQuery();
             BeanUtils.copyProperties(userSubscribeDetailDo, sellHouseBeSureToSnatchDoQuery);
             sellHouseBeSureToSnatchDoQuery.setIsNew(1);
+            sellHouseBeSureToSnatchDoQuery.setPageSize(pageSize);
+            sellHouseBeSureToSnatchDoQuery.setPageNum(pageIndex);
             SellHouseBeSureToSnatchDomain sellHouseBeSureToSnatchDos = sellHouseService.getBeSureToSnatchList(sellHouseBeSureToSnatchDoQuery);
             return sellHouseBeSureToSnatchDos.getTotalCount();
-        }
-        else if(userSubscribeDetailDo.getTopicType() == 1) {
+        } else if (userSubscribeDetailDo.getTopicType() == 1) {
 //            SellHouseBeSureToSnatchDoQuery sellHouseBeSureToSnatchDoQuery = new SellHouseBeSureToSnatchDoQuery();
 //            BeanUtils.copyProperties(userSubscribeDetailDo, sellHouseBeSureToSnatchDoQuery);
 //            sellHouseBeSureToSnatchDoQuery.setIsNew(1);
 //            SellHouseBeSureToSnatchDomain sellHouseBeSureToSnatchDos = sellHouseService.getBeSureToSnatchList(sellHouseBeSureToSnatchDoQuery);
 //            return sellHouseBeSureToSnatchDos.getTotalCount();
-        }
-        else if(userSubscribeDetailDo.getTopicType() == 2) {
+        } else if (userSubscribeDetailDo.getTopicType() == 2) {
 //            SellHouseBeSureToSnatchDoQuery sellHouseBeSureToSnatchDoQuery = new SellHouseBeSureToSnatchDoQuery();
 //            BeanUtils.copyProperties(userSubscribeDetailDo, sellHouseBeSureToSnatchDoQuery);
 //            sellHouseBeSureToSnatchDoQuery.setIsNew(1);
@@ -109,29 +117,28 @@ public class SubscribeServiceImpl implements SubscribeService {
         return 0L;
     }
 
-    private Object getHouseListBySubscribe(UserSubscribeDetailDo userSubscribeDetailDo)
-    {
+    private Object getHouseListBySubscribe(UserSubscribeDetailDo userSubscribeDetailDo) {
         Integer pageIndex = 1;
-        Integer pageSize = 10;
-        if(userSubscribeDetailDo.getTopicType() == 3) {
+        Integer pageSize = 5;
+        if (userSubscribeDetailDo.getTopicType() == 3) {
             SellHouseBeSureToSnatchDoQuery sellHouseBeSureToSnatchDoQuery = new SellHouseBeSureToSnatchDoQuery();
             BeanUtils.copyProperties(userSubscribeDetailDo, sellHouseBeSureToSnatchDoQuery);
+            sellHouseBeSureToSnatchDoQuery.setPageSize(pageSize);
+            sellHouseBeSureToSnatchDoQuery.setPageNum(pageIndex);
             return sellHouseService.getBeSureToSnatchList(sellHouseBeSureToSnatchDoQuery);
+        } else if (userSubscribeDetailDo.getTopicType() == 1) {
+            CutPriceShellHouseDoQuery cutPriceShellHouseDoQuery = new CutPriceShellHouseDoQuery();
+            BeanUtils.copyProperties(userSubscribeDetailDo, cutPriceShellHouseDoQuery);
+            cutPriceShellHouseDoQuery.setPageSize(pageSize);
+            cutPriceShellHouseDoQuery.setPageNum(pageIndex);
+            return cutPriceSellHouseRestService.getCutPriceHouse(cutPriceShellHouseDoQuery);
+        } else if (userSubscribeDetailDo.getTopicType() == 2) {
+            LowerPriceShellHouseDoQuery lowerPriceShellHouseDoQuery = new LowerPriceShellHouseDoQuery();
+            BeanUtils.copyProperties(userSubscribeDetailDo, lowerPriceShellHouseDoQuery);
+            lowerPriceShellHouseDoQuery.setPageSize(pageSize);
+            lowerPriceShellHouseDoQuery.setPageNum(pageIndex);
+            return lowerPriceSellHouseRestService.getLowerPriceHouse(lowerPriceShellHouseDoQuery);
         }
-        else if(userSubscribeDetailDo.getTopicType() == 1) {
-//            SellHouseBeSureToSnatchDoQuery sellHouseBeSureToSnatchDoQuery = new SellHouseBeSureToSnatchDoQuery();
-//            BeanUtils.copyProperties(userSubscribeDetailDo, sellHouseBeSureToSnatchDoQuery);
-//            sellHouseBeSureToSnatchDoQuery.setIsNew(1);
-//            SellHouseBeSureToSnatchDomain sellHouseBeSureToSnatchDos = sellHouseService.getBeSureToSnatchList(sellHouseBeSureToSnatchDoQuery);
-//            return sellHouseBeSureToSnatchDos.getTotalCount();
-        }
-        else if(userSubscribeDetailDo.getTopicType() == 2) {
-//            SellHouseBeSureToSnatchDoQuery sellHouseBeSureToSnatchDoQuery = new SellHouseBeSureToSnatchDoQuery();
-//            BeanUtils.copyProperties(userSubscribeDetailDo, sellHouseBeSureToSnatchDoQuery);
-//            sellHouseBeSureToSnatchDoQuery.setIsNew(1);
-//            SellHouseBeSureToSnatchDomain sellHouseBeSureToSnatchDos = sellHouseService.getBeSureToSnatchList(sellHouseBeSureToSnatchDoQuery);
-//            return sellHouseBeSureToSnatchDos.getTotalCount();
-        }
-        return 0L;
+        return null;
     }
 }
