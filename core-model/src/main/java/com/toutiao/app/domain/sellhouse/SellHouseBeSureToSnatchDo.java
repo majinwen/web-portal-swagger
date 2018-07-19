c2bf2a10ed3cf691e5404dfdb37f4ff60b389889
lile <lile@toutiaofangchan.com>
2018-07-19 11:17:52 +0800

    修改bug

--- core-model/src/main/java/com/toutiao/app/domain/sellhouse/SellHouseBeSureToSnatchDo.java
+++ core-model/src/main/java/com/toutiao/app/domain/sellhouse/SellHouseBeSureToSnatchDo.java
@@ -224,7 +224,7 @@ public class SellHouseBeSureToSnatchDo {
     /**
      * 标签
      */
-    @ChangeName("tags")
+    @ChangeName("tagsName")
     private String[] tagsName;
 
     private String houseTitle;
