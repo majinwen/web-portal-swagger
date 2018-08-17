55f24549cc33297987c11c3b54cece043457d8af
zengqingzhou <zengqingzhou@123.com>
2018-08-17 12:12:12 +0800

    首页购房报告

--- api-impl/src/main/java/com/toutiao/web/apiimpl/rest/Intelligence/HomePageReportController.java
+++ api-impl/src/main/java/com/toutiao/web/apiimpl/rest/Intelligence/HomePageReportController.java
@@ -19,7 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/{citypath}/findhouse/byHouseReport")
+@RequestMapping("/rest/findhouse/byHouseReport")
 public class HomePageReportController {
     @Autowired
     private IntelligenceFhTdService intelligenceFhTdService;
