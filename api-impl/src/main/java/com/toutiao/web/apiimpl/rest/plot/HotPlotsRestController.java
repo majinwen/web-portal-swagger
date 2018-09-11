ebb34a0b335e936aca04981d560522380ff7bcbe
jiangweilong <jiangweilongowen@163.com>
2018-09-11 18:30:46 +0800

    热门小区

--- api-impl/src/main/java/com/toutiao/web/apiimpl/rest/plot/HotPlotsRestController.java
+++ api-impl/src/main/java/com/toutiao/web/apiimpl/rest/plot/HotPlotsRestController.java
@@ -6,6 +6,7 @@ import com.toutiao.app.domain.hotplot.SearchHotProjDo;
 import com.toutiao.app.domain.hotplot.SearchHotProjDomain;
 import com.toutiao.app.service.plot.HotPlotsRestService;
 import com.toutiao.web.common.restmodel.NashResult;
+import com.toutiao.web.common.util.StringTool;
 import com.toutiao.web.common.util.city.CityUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,8 +34,11 @@ public class HotPlotsRestController {
     @RequestMapping(value = "/getHotPlotByCityId")
     @ResponseBody
     public NashResult getSuggestByKeyword(){
-//        String city = "12";
-        SearchHotProjDomain searchHotProjDomain = hotPlotsRestService.getHotPlotsByCityId(CityUtils.getCity());
+        String city = "";
+        if(StringTool.isNotEmpty(CityUtils.getCity())){
+            city = CityUtils.returnCityId(CityUtils.getCity()).toString();
+        }
+        SearchHotProjDomain searchHotProjDomain = hotPlotsRestService.getHotPlotsByCityId(city);
         HotPlotListResponse hotPlotListResponse = new HotPlotListResponse();
         BeanUtils.copyProperties(searchHotProjDomain,hotPlotListResponse);
 
