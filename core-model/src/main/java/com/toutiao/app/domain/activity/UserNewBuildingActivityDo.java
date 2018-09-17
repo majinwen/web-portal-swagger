959a891e6069549718156f87382cc03a2afb2c31
jiangweilong <jiangweilongowen@163.com>
2018-09-17 10:50:30 +0800

    优惠楼盘新增/查询

---
+++ core-model/src/main/java/com/toutiao/app/domain/activity/UserNewBuildingActivityDo.java
@@ -0,0 +1,54 @@
+package com.toutiao.app.domain.activity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class UserNewBuildingActivityDo {
+    /**
+     * 用户活动id
+     */
+    private Integer id;
+
+    /**
+     * 用户id
+     */
+    private Integer userId;
+
+    /**
+     * 用户手机号
+     */
+    private String userPhone;
+
+    /**
+     * 用户昵称
+     */
+    private String userNickname;
+
+    /**
+     * 参与活动楼盘id
+     */
+    private Integer activityBuildingId;
+
+    /**
+     * 参与活动楼盘名称
+     */
+    private String activityBuildingName;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 城市代码
+     */
+    private Integer cityId;
+
+    /**
+     * 活动id
+     */
+    private Integer activityId;
+
+}
---
+++ core-service/src/main/java/com/toutiao/app/service/activity/NewHouseActivityRestService.java
@@ -0,0 +1,31 @@
+package com.toutiao.app.service.activity;
+
+import com.github.pagehelper.PageInfo;
+import com.toutiao.app.domain.activity.UserNewBuildingActivityDo;
+import com.toutiao.app.domain.activity.UserNewBuildingActivityDoQuery;
+import com.toutiao.web.common.restmodel.NashResult;
+
+/**
+ * Created by IntelliJ Idea
+ * Author: Jiang Weilong
+ * Date:   2018-09-14
+ * Time:   14:22
+ * Theme:
+ */
+public interface NewHouseActivityRestService {
+
+    /**
+     * 表单提交--用户提交参与的活动楼盘信息
+     * @param userNewBuildingActivityDoQuery
+     * @return
+     */
+    NashResult saveUserActivityMsg(UserNewBuildingActivityDoQuery userNewBuildingActivityDoQuery);
+
+    /**
+     * 查询用户参与新房活动信息
+     * @param userNewBuildingActivityDoQuery
+     * @return
+     */
+    PageInfo<UserNewBuildingActivityDo> listUserActivityMsg(UserNewBuildingActivityDoQuery userNewBuildingActivityDoQuery);
+
+}
