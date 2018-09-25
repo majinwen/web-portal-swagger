2498f748cc698c3020f834d19ab63dc8e6884cd7
zhaiyanming <zhaiyanming1992@163.com>
2018-09-25 18:17:49 +0800

    邀请码后台管理

---
+++ core-model/src/main/java/com/toutiao/app/domain/invitation/SuperInviteHistoryDo.java
@@ -0,0 +1,57 @@
+package com.toutiao.app.domain.invitation;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class SuperInviteHistoryDo {
+    /**
+     * Id
+     */
+    private Integer id;
+
+    /**
+     * 一级邀请码
+     */
+    private String firstLevelCode;
+
+    /**
+     * 二级邀请码
+     */
+    private String secondLevelCode;
+
+    /**
+     * 三级邀请码
+     */
+    private String thirdLevelCode;
+
+    /**
+     * 用户Id
+     */
+    private String userId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 设备号
+     */
+    private String equipmentNo;
+
+    /**
+     * 用户手机号
+     */
+    private String phone;
+
+    /**
+     * 受邀人邀请码
+     */
+    private String yourCode;
+
+    private Short isValid;
+
+    private Date payTime;
+}
