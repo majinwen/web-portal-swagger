dee8f4ab93d0930ec3b9c018207ca30dbb8c7404
jiangweilong <jiangweilongowen@163.com>
2018-10-12 15:32:38 +0800

    区域商圈

--- core-service/src/main/java/com/toutiao/app/service/rent/impl/RentRestRestServiceImpl.java
+++ core-service/src/main/java/com/toutiao/app/service/rent/impl/RentRestRestServiceImpl.java
@@ -549,23 +549,23 @@ public class RentRestRestServiceImpl implements RentRestService {
             BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
             if(StringUtil.isNotNullString(DistrictMap.getDistricts(rentHouseDoQuery.getKeyword()))){
                 queryBuilder
-                        .should(QueryBuilders.matchQuery("zufang_name", rentHouseDoQuery.getKeyword()))
-                        .should(QueryBuilders.matchQuery("area_name_search", rentHouseDoQuery.getKeyword()).analyzer("ik_smart"))
-                        .should(QueryBuilders.matchQuery("district_name_search", rentHouseDoQuery.getKeyword()).analyzer("ik_smart").boost(2))
-                        .should(QueryBuilders.matchQuery("zufang_name_search", rentHouseDoQuery.getKeyword()).analyzer("ik_smart"));
+                        .should(QueryBuilders.matchQuery("zufang_name", rentHouseDoQuery.getKeyword()).operator(Operator.AND))
+                        .should(QueryBuilders.matchQuery("area_name_search", rentHouseDoQuery.getKeyword()).operator(Operator.AND).analyzer("ik_smart"))
+                        .should(QueryBuilders.matchQuery("district_name_search", rentHouseDoQuery.getKeyword()).operator(Operator.AND).analyzer("ik_smart").boost(2))
+                        .should(QueryBuilders.matchQuery("zufang_name_search", rentHouseDoQuery.getKeyword()).operator(Operator.AND).analyzer("ik_smart"));
             }else if(StringUtil.isNotNullString(AreaMap.getAreas(rentHouseDoQuery.getKeyword()))){
                 queryBuilder
-                        .should(QueryBuilders.matchQuery("area_name_search", rentHouseDoQuery.getKeyword()).analyzer("ik_smart").boost(2))
-                        .should(QueryBuilders.matchQuery("zufang_name", rentHouseDoQuery.getKeyword()))
-                        .should(QueryBuilders.matchQuery("district_name_search", rentHouseDoQuery.getKeyword()).analyzer("ik_smart"))
-                        .should(QueryBuilders.matchQuery("zufang_name_search", rentHouseDoQuery.getKeyword()).analyzer("ik_smart").boost(2));
+                        .should(QueryBuilders.matchQuery("area_name_search", rentHouseDoQuery.getKeyword()).operator(Operator.AND).analyzer("ik_smart").boost(2))
+                        .should(QueryBuilders.matchQuery("zufang_name", rentHouseDoQuery.getKeyword()).operator(Operator.AND))
+                        .should(QueryBuilders.matchQuery("district_name_search", rentHouseDoQuery.getKeyword()).operator(Operator.AND).analyzer("ik_smart"))
+                        .should(QueryBuilders.matchQuery("zufang_name_search", rentHouseDoQuery.getKeyword()).operator(Operator.AND).analyzer("ik_smart").boost(2));
             }else {
                 queryBuilder
-                        .should(QueryBuilders.matchQuery("zufang_name", rentHouseDoQuery.getKeyword()).boost(2))
+                        .should(QueryBuilders.matchQuery("zufang_name", rentHouseDoQuery.getKeyword()).operator(Operator.AND).boost(2))
                         .should(QueryBuilders.matchQuery("zufang_nickname",rentHouseDoQuery.getKeyword()).fuzziness("AUTO").operator(Operator.AND))
-                        .should(QueryBuilders.matchQuery("area_name_search", rentHouseDoQuery.getKeyword()))
-                        .should(QueryBuilders.matchQuery("district_name_search", rentHouseDoQuery.getKeyword()))
-                        .should(QueryBuilders.matchQuery("zufang_name_search", rentHouseDoQuery.getKeyword()));
+                        .should(QueryBuilders.matchQuery("area_name_search", rentHouseDoQuery.getKeyword()).operator(Operator.AND))
+                        .should(QueryBuilders.matchQuery("district_name_search", rentHouseDoQuery.getKeyword()).operator(Operator.AND))
+                        .should(QueryBuilders.matchQuery("zufang_name_search", rentHouseDoQuery.getKeyword()).operator(Operator.AND));
             }
             boolQueryBuilder.must(queryBuilder);
         }
