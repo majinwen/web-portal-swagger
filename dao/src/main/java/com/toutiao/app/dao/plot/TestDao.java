be7fd098785e3a6fd9480ac475507e361f7e5d79
jiangweilong <jiangweilong@bidewu.com>
2018-11-22 10:01:03 +0800

    demo new http client2

--- dao/src/main/java/com/toutiao/app/dao/plot/TestDao.java
+++
@@ -1,9 +0,0 @@
-package com.toutiao.app.dao.plot;
-
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-
-public interface TestDao {
-
-    SearchResponse search(SearchRequest searchRequest) throws Exception;
-}
