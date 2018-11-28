db25418c2954fff68330eb8860ee8f309559d5d2
cuishihao <915701453@qq.com>
2018-11-28 17:22:31 +0800

    地铁找房

--- core-model/src/main/java/com/toutiao/app/domain/mapSearch/EsfMapSearchDomain.java
+++ core-model/src/main/java/com/toutiao/app/domain/mapSearch/EsfMapSearchDomain.java
@@ -1,5 +1,6 @@
 package com.toutiao.app.domain.mapSearch;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -17,10 +18,12 @@ public class EsfMapSearchDomain {
     /**
      * 当前可视界面描述
      */
+    @ApiModelProperty("当前可视界面描述")
     private String hit;
 
     /**
      * 返回数据
      */
+    @ApiModelProperty("返回的数据")
     private List<EsfMapSearchDo> data;
 }
