1796911349847ededa3b70b9fb38c0f075f62dd2
jiangweilong <jiangweilong@bidewu.com>
2018-12-01 15:00:04 +0800

    123

--- dao/src/main/java/com/toutiao/app/dao/mapsearch/impl/NewHouseMapSearchEsDaoImpl.java
+++ dao/src/main/java/com/toutiao/app/dao/mapsearch/impl/NewHouseMapSearchEsDaoImpl.java
@@ -59,7 +59,7 @@ public class NewHouseMapSearchEsDaoImpl implements NewHouseMapSearchEsDao {
         searchSourceBuilder.query(boolQueryBuilder).size(50)
                 .fetchSource(new String[]{"district_id","district_name","property_type",
                         "building_name","building_name_id","average_price","sale_status_name","building_title_img"
-                        ,"totalPrice","is_active","building_tags","house_max_area","house_min_area","activity_info"},
+                        ,"totalPrice","is_active","building_tags","house_max_area","house_min_area","activity_info","deliver_time"},
                         null);
         searchRequest.source(searchSourceBuilder);
 
