8706cf4a657a8acb48dd5244a55783cd62389112
pirate-noland <pirate_noland@163.com>
2018-12-01 13:00:46 +0800

    二手房标签排序

--- core-model/src/main/java/com/toutiao/app/domain/sellhouse/HouseSubject.java
+++ core-model/src/main/java/com/toutiao/app/domain/sellhouse/HouseSubject.java
@@ -11,4 +11,12 @@ public class HouseSubject {
 
     @ApiModelProperty(value = "链接", name = "url")
     private String url;
+
+    public HouseSubject(String text, String url) {
+        this.text = text;
+        this.url = url;
+    }
+
+    public HouseSubject() {
+    }
 }
