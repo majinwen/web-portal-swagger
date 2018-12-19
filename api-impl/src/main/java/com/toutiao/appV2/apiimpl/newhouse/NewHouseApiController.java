bff60334f92b1b2e630ab383d02208495ab67e0c
zhaiyanming <zhaiyanming1992@163.com>
2018-12-19 11:01:41 +0800

    新房广告位

--- api-impl/src/main/java/com/toutiao/appV2/apiimpl/newhouse/NewHouseApiController.java
+++ api-impl/src/main/java/com/toutiao/appV2/apiimpl/newhouse/NewHouseApiController.java
@@ -1,9 +1,12 @@
 package com.toutiao.appV2.apiimpl.newhouse;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageInfo;
 import com.toutiao.app.api.chance.request.activity.NewHouseActivityRequest;
+import com.toutiao.app.api.chance.request.advertisement.AdNewHouse;
+import com.toutiao.app.api.chance.response.homepage.HomePageNewHouseResponse;
 import com.toutiao.app.api.chance.response.newhouse.NewHouseDetailResponse;
 import com.toutiao.app.api.chance.response.newhouse.NewHouseLayoutCountResponse;
 import com.toutiao.app.api.chance.response.newhouse.NewHouseListDomainResponse;
@@ -16,6 +19,7 @@ import com.toutiao.app.domain.activity.UserNewBuildingActivityDoQuery;
 import com.toutiao.app.domain.newhouse.*;
 import com.toutiao.app.domain.user.UserBasicDo;
 import com.toutiao.app.service.activity.NewHouseActivityRestService;
+import com.toutiao.app.service.advertisement.AdNewHouseRestService;
 import com.toutiao.app.service.newhouse.NewHouseLayoutService;
 import com.toutiao.app.service.newhouse.NewHouseRestService;
 import com.toutiao.app.service.newhouse.NewHouseTopicsRestService;
@@ -25,10 +29,8 @@ import com.toutiao.appV2.model.PageRequest;
 import com.toutiao.appV2.model.StringDataResponse;
 import com.toutiao.appV2.model.newhouse.*;
 import com.toutiao.appV2.model.userbasic.UserLoginResponse;
-import com.toutiao.web.apiimpl.authentication.User;
 import com.toutiao.web.common.assertUtils.First;
 import com.toutiao.web.common.assertUtils.Second;
-import com.toutiao.web.common.restmodel.NashResult;
 import com.toutiao.web.common.util.CookieUtils;
 import com.toutiao.web.common.util.JSONUtil;
 import com.toutiao.web.common.util.StringTool;
@@ -69,6 +71,9 @@ public class NewHouseApiController implements NewHouseApi {
 
     @Autowired
     private NewHouseActivityRestService newHouseActivityRestService;
+
+    @Autowired
+    private AdNewHouseRestService adNewHouseRestService;
 
     @Autowired
     private UserBasicInfoService userBasicInfoService;
@@ -275,6 +280,23 @@ public class NewHouseApiController implements NewHouseApi {
         newHouseGuessLikeResponse.setTotalNum(newHouseListDomain.getTotalCount());
         log.info("返回结果集:{}", JSONUtil.stringfy(newHouseGuessLikeResponse));
         return new ResponseEntity<>(newHouseGuessLikeResponse, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<NewHouseListDomainResponse> getAdNewHouseListByIds(@Validated(First.class) AdNewHouse adNewHouse) {
+        NewHouseListDomainResponse newHouseListDomainResponse = new NewHouseListDomainResponse();
+        NewHouseListDomain newHouseListDomain = adNewHouseRestService.getAdNewHouseList(adNewHouse.getNewHouseIds(), CityUtils.getCity());
+        BeanUtils.copyProperties(newHouseListDomain, newHouseListDomainResponse);
+        return new ResponseEntity<>(newHouseListDomainResponse, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<AdRecommendNewRespose> getAdRecommendNewHouseByIds(AdNewHouse adNewHouse) {
+        NewHouseListDomain adNewHouseHomePage = adNewHouseRestService.getAdNewHouseHomePage(adNewHouse.getNewHouseIds(), CityUtils.getCity());
+        JSONArray json = JSONArray.parseArray(JSON.toJSONString(adNewHouseHomePage.getData()));
+        List<HomePageNewHouseResponse> newHouseListResponses = JSONObject.parseArray(json.toJSONString(), HomePageNewHouseResponse.class);
+        AdRecommendNewRespose response = AdRecommendNewRespose.builder().data(newHouseListResponses).totalNum(newHouseListResponses.size()).build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
