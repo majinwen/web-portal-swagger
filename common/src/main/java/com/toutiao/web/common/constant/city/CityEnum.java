5907b96cfed8a883c94f6057cfd6d7a89490ece9
zhaiyanming <zhaiyanming1992@163.com>
2018-12-27 12:50:40 +0800

    消息推送，城市管理

--- common/src/main/java/com/toutiao/web/common/constant/city/CityEnum.java
+++ common/src/main/java/com/toutiao/web/common/constant/city/CityEnum.java
@@ -14,23 +14,25 @@ public enum  CityEnum implements IntBaseType {
     /**
      * 城市枚举
      */
-    BEIJING(CityConstant.ABBREVIATION_BEIJING,CityConstant.CODE_BEIJING),
-    SHANGHAI(CityConstant.ABBREVIATION_SHANGHAI,CityConstant.CODE_SHANGHAI),
-    TIANJIN(CityConstant.ABBREVIATION_TIANJIN,CityConstant.CODE_TIANJIN),
-    SUZHOU(CityConstant.ABBREVIATION_SUZHOU,CityConstant.CODE_SUZHOU),
-    HANGZHOU(CityConstant.ABBREVIATION_HANGZHOU,CityConstant.CODE_HANGZHOU),
-    GUANGZHOU(CityConstant.ABBREVIATION_GUANGZHOU,CityConstant.CODE_GUANGZHOU),
-    SHENZHEN(CityConstant.ABBREVIATION_SHENZHEN,CityConstant.CODE_SHENZHEN);
+    BEIJING(CityConstant.ABBREVIATION_BEIJING,CityConstant.CODE_BEIJING,CityConstant.NAME_BEIJING),
+    SHANGHAI(CityConstant.ABBREVIATION_SHANGHAI,CityConstant.CODE_SHANGHAI,CityConstant.NAME_SHANGHAI),
+    TIANJIN(CityConstant.ABBREVIATION_TIANJIN,CityConstant.CODE_TIANJIN,CityConstant.NAME_TIANJIN),
+    SUZHOU(CityConstant.ABBREVIATION_SUZHOU,CityConstant.CODE_SUZHOU,CityConstant.NAME_SUZHOU),
+    HANGZHOU(CityConstant.ABBREVIATION_HANGZHOU,CityConstant.CODE_HANGZHOU,CityConstant.NAME_HANGZHOU),
+    GUANGZHOU(CityConstant.ABBREVIATION_GUANGZHOU,CityConstant.CODE_GUANGZHOU,CityConstant.NAME_GUANGZHOU),
+    SHENZHEN(CityConstant.ABBREVIATION_SHENZHEN,CityConstant.CODE_SHENZHEN,CityConstant.NAME_SHENZHEN);
 
 
 
 
     private String desc;
     private int value;
+    private String name;
 
-    CityEnum(String desc, int value) {
+    CityEnum(String desc, int value, String name) {
         this.desc = desc;
         this.value = value;
+        this.name = name;
     }
 
 
@@ -56,6 +58,17 @@ public enum  CityEnum implements IntBaseType {
     }
 
 
+    public static String getName(Integer id) {
+        CityEnum[] cityEnums = values();
+        for (CityEnum cityEnum : cityEnums) {
+            if (cityEnum.value==id) {
+                return cityEnum.name;
+            }
+        }
+        return null;
+    }
+
+
 
     @Override
     public int getValue() {
@@ -66,4 +79,10 @@ public enum  CityEnum implements IntBaseType {
     public String getDesc() {
         return desc;
     }
+
+
+    public String getName() {
+        return name;
+    }
+
 }
--- common/src/main/java/com/toutiao/web/common/util/city/CityUtils.java
+++ common/src/main/java/com/toutiao/web/common/util/city/CityUtils.java
@@ -67,4 +67,12 @@ public class CityUtils {
     }
 
 
+    public static String retuenCityName(Integer cityId){
+        if(StringTool.isBlank(cityId)){
+            return CityEnum.BEIJING.getName();
+        }else{
+            return CityEnum.getName(cityId);
+        }
+    }
+
 }
