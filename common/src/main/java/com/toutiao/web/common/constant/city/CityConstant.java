bb42ba4341ee7c740e5e04d10d50e356e15ac062
wangkai <wangkai112138@163.com>
2018-12-29 14:10:13 +0800

    新上城市添加枚举

--- common/src/main/java/com/toutiao/web/common/constant/city/CityConstant.java
+++ common/src/main/java/com/toutiao/web/common/constant/city/CityConstant.java
@@ -28,6 +28,28 @@ public class CityConstant {
 
     public static final String ABBREVIATION_SHENZHEN= "sz";
 
+    public static final String ABBREVIATION_CHONGQIN= "cq";
+    public static final String ABBREVIATION_ZHUHAI= "zh";
+    public static final String ABBREVIATION_DONGGUAN= "dg";
+    public static final String ABBREVIATION_CHENGDU= "cd";
+    public static final String ABBREVIATION_SHENYANG= "sy";
+    public static final String ABBREVIATION_NANJING= "nj";
+    public static final String ABBREVIATION_WUXI= "wuxi";
+    public static final String ABBREVIATION_FUZHOU= "fz";
+    public static final String ABBREVIATION_CHANGCHUN= "changchun";
+
+    public static final String ABBREVIATION_ZHENGZHOU= "zz";
+    public static final String ABBREVIATION_JINAN= "jn";
+    public static final String ABBREVIATION_QINGDAO= "qd";
+    public static final String ABBREVIATION_YANTAI= "yt";
+    public static final String ABBREVIATION_HEFEI= "hf";
+    public static final String ABBREVIATION_NANNING= "nn";
+    public static final String ABBREVIATION_TAIYUAN= "ty";
+    public static final String ABBREVIATION_XIAN= "xian";
+    public static final String ABBREVIATION_WUHAN= "wh";
+    public static final String ABBREVIATION_CHANGSHA= "cs";
+    public static final String ABBREVIATION_NANCHANG= "nc";
+
 
 
 
@@ -52,6 +74,28 @@ public class CityConstant {
 
     public static final Integer CODE_SHENZHEN = 17;//深圳
 
+    public static final Integer CODE_CHONGQIN= 15;
+    public static final Integer CODE_ZHUHAI= 18;
+    public static final Integer CODE_DONGGUAN= 24;
+    public static final Integer CODE_CHENGDU= 45;
+    public static final Integer CODE_SHENYANG= 55;
+    public static final Integer CODE_NANJING= 65;
+    public static final Integer CODE_WUXI= 66;
+    public static final Integer CODE_FUZHOU= 75;
+    public static final Integer CODE_CHANGCHUN= 84;
+
+    public static final Integer CODE_ZHENGZHOU= 103;
+    public static final Integer CODE_JINAN= 113;
+    public static final Integer CODE_QINGDAO= 114;
+    public static final Integer CODE_YANTAI= 118;
+    public static final Integer CODE_HEFEI= 123;
+    public static final Integer CODE_NANNING= 142;
+    public static final Integer CODE_TAIYUAN= 155;
+    public static final Integer CODE_XIAN= 176;
+    public static final Integer CODE_WUHAN= 194;
+    public static final Integer CODE_CHANGSHA= 204;
+    public static final Integer CODE_NANCHANG= 214;
+
 
     /**
      * 城市名称
@@ -71,6 +115,28 @@ public class CityConstant {
     public static final String NAME_GUANGZHOU= "广州";
 
     public static final String NAME_SHENZHEN = "深圳";
+
+    public static final String NAME_CHONGQIN= "重庆";
+    public static final String NAME_ZHUHAI= "珠海";
+    public static final String NAME_DONGGUAN= "东莞";
+    public static final String NAME_CHENGDU= "成都";
+    public static final String NAME_SHENYANG= "沈阳";
+    public static final String NAME_NANJING= "南京";
+    public static final String NAME_WUXI= "无锡";
+    public static final String NAME_FUZHOU= "福州";
+    public static final String NAME_CHANGCHUN= "长春";
+
+    public static final String NAME_ZHENGZHOU= "郑州";
+    public static final String NAME_JINAN= "济南";
+    public static final String NAME_QINGDAO= "青岛";
+    public static final String NAME_YANTAI= "烟台";
+    public static final String NAME_HEFEI= "合肥";
+    public static final String NAME_NANNING= "南宁";
+    public static final String NAME_TAIYUAN= "太原";
+    public static final String NAME_XIAN= "西安";
+    public static final String NAME_WUHAN= "武汉";
+    public static final String NAME_CHANGSHA= "长沙";
+    public static final String NAME_NANCHANG= "南昌";
 
 
 }
