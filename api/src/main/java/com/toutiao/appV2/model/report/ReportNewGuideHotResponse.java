47484b82144a3ba3e317df10480964dfab0ca3c2
pirate-noland <pirate_noland@163.com>
2019-01-02 15:40:16 +0800

    数据报告，添加 均价、总价 合并后 的价格

--- api/src/main/java/com/toutiao/appV2/model/report/ReportNewGuideHotResponse.java
+++ api/src/main/java/com/toutiao/appV2/model/report/ReportNewGuideHotResponse.java
@@ -81,4 +81,9 @@ public class ReportNewGuideHotResponse {
         this.averagePrice = Double.parseDouble(doubleStr);
     }
 
+    /**
+     * 总价
+     */
+    @ApiModelProperty(value = "总价")
+    private Double totalPrice;
 }
